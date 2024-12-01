1546cddbeb070885f9b012ce9a143050f375683d
smails <maxon2504@gmail.com>
2024-12-01 18:39:31 +0300

    подбор товара

---
+++ src/modules/catalog/catalog.js
@@ -0,0 +1,35 @@
+import $ from 'jquery'
+import Swiper from 'swiper';
+import { Pagination } from 'swiper/modules';
+
+
+$('.prod-item__middle').each((index, item) => {
+  const swiper = new Swiper($(item).find('.swiper')[0], {
+    loop: true,
+    slidesPerView: 1,
+    pagination: {
+      el: '.swiper-dots',
+      type: 'bullets',
+    },
+    modules: [Pagination],
+  });
+})
+
+
+$('.catalog-filter__all-filter').click(() => {
+  $('.modal-filter').fadeIn();
+})
+
+$('.modal-filter__close').click(() => {
+  $('.modal-filter').hide();
+})
+
+$('.catalog-filter__open').click(function () {
+  if($(this).hasClass('open')){
+    $(this).removeClass('open').find('span').text('Раскрыть фильтр');
+    $('.catalog-filter__bottom').slideUp();
+    return;
+  }
+  $(this).addClass('open').find('span').text('Скрыть фильтр');
+  $('.catalog-filter__bottom').slideDown();
+})
